2e2b1247334d0ddcb849f2aded75f0ee60815ca7
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-09-02 15:13:07 +0530

    Create SumCalculator.js

---
+++ src/components/SumCalculator.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+export default function SumcalculatorOriginal() {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let totalSum = numbers.reduce((acc, num) => acc + num, 0);
+      setSum(totalSum);
+    }, 0);
+  }, [numbers]);
+
+  function handleChange(e) {
+    const val = Number(e.target.value);
+    if (!isNaN(val)) {
+      setNumbers((prev) => [...prev, val]);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Sum Calculator</h1>
+      <input type="number" onChange={handleChange} />
+      <p>Sum: {sum}</p>
+      {/* <p>Numbers: {numbers.join(", ")}</p> */}
+    </div>
+  );
+}
