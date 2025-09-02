a9957ab33d55dfce6ddf46f8d096446bde05136e
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-09-02 15:27:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import './../components/SumCalculator.js';
+import SumCalculator from './SumCalculator.js';
 
 const App = () => {
   return (
